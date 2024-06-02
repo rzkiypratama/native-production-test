f01fbb83b6ef581b1a439c96a17ab4e81d0d64a8
rzkiypratama <pratamabusiness@gmail.com>
2024-06-02 19:56:42 +0800

    remove unused data type

--- types/productTypes.ts
+++ types/productTypes.ts
@@ -1,30 +1,26 @@
-export interface Category {
-  id: number;
-  name: string;
-  image: string;
-  creationAt?: string;
-  updatedAt?: string;
-}
-
+// types/productTypes.ts
 export interface Product {
   id: number;
   title: string;
   price: number;
-  description?: string;
-  images?: string[];
-  creationAt?: string;
-  updatedAt?: string;
-  category?: Category;
+  description: string;
+  categoryId: number;
+  images: string[];
+  creationAt: string;
+  updatedAt: string;
+  category: {
+    id: number;
+    name: string;
+  };
 }
 
-
-  export type EditableCellProps = {
-    title: React.ReactNode;
-    editable: boolean;
-    children: React.ReactNode;
-    dataIndex: keyof Product;
-    record: Product;
-    inputType: 'number' | 'text';
-    index: number;
-    editing: boolean;
-  };
+export type EditableCellProps = {
+  title: React.ReactNode;
+  editable: boolean;
+  children: React.ReactNode;
+  dataIndex: keyof Product;
+  record: Product;
+  inputType: "number" | "text";
+  index: number;
+  editing: boolean;
+};
