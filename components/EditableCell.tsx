fc171e676581f6399803808b16d61fa79dbf52ba
rzkiypratama <pratamabusiness@gmail.com>
2024-06-02 16:10:49 +0800

    feat: add editable component

---
+++ components/EditableCell.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Form, Input, InputNumber } from 'antd';
+
+interface EditableCellProps {
+  editing: boolean;
+  dataIndex: string;
+  title: string;
+  inputType: 'number' | 'text';
+  record: any;
+  index: number;
+  children: React.ReactNode;
+}
+
+const EditableCell: React.FC<EditableCellProps> = ({
+  editing,
+  dataIndex,
+  title,
+  inputType,
+  record,
+  index,
+  children,
+  ...restProps
+}) => {
+  const inputNode = inputType === 'number' ? <InputNumber /> : <Input />;
+  return (
+    <td {...restProps}>
+      {editing ? (
+        <Form.Item
+          name={dataIndex}
+          style={{ margin: 0 }}
+          rules={[{ required: true, message: `Please Input ${title}!` }]}
+        >
+          {inputNode}
+        </Form.Item>
+      ) : (
+        children
+      )}
+    </td>
+  );
+};
+
+export default EditableCell;
