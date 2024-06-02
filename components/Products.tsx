15b9827b063b3691128f299aeb25f55beeee71cb
rzkiypratama <pratamabusiness@gmail.com>
2024-06-02 16:12:49 +0800

    feat: add editing table data feature

--- components/Products.tsx
+++ components/Products.tsx
@@ -1,26 +1,28 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import { Table, Button, Modal, Spin } from 'antd';
+import { Table, Button, Modal, Spin, Form, Typography, Popconfirm, Badge } from 'antd';
 import { useProductStore } from '../store/useProducts';
 import { useCartStore } from '../store/cartStore';
 import ProductForm from './ProductsForm';
-import { Product } from '../types/product';
+import { Product } from '../types/productTypes';
 import { ShoppingCartOutlined } from '@ant-design/icons';
 import Cart from './Cart';
+import EditableCell from './EditableCell';
 
 const ProductsComponent: React.FC = () => {
-  const { products, fetchProducts, addProduct, isLoading } = useProductStore();
-  const { cart, addToCart, initializeCart } = useCartStore();
+  const { products, fetchProducts, addProduct, editProduct, isLoading } = useProductStore();
+  const { addToCart, initializeCart, cart } = useCartStore();
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [isCartVisible, setIsCartVisible] = useState(false);
+  const [form] = Form.useForm();
+  const [editingKey, setEditingKey] = useState<number | ''>('');
 
   useEffect(() => {
     fetchProducts();
     initializeCart();
   }, [fetchProducts, initializeCart]);
 
-  // create new product modal button
   const handleCreate = () => {
     setIsModalVisible(true);
   };
@@ -29,15 +31,13 @@ const ProductsComponent: React.FC = () => {
     setIsModalVisible(false);
   };
 
-  // cart modal button
   const handleCart = () => {
     setIsCartVisible(true);
-  }
+  };
 
   const handleCartClose = () => {
     setIsCartVisible(false);
-  }
-
+  };
 
   const handleFormSubmit = async (data: Omit<Product, 'id'>) => {
     try {
@@ -52,26 +52,47 @@ const ProductsComponent: React.FC = () => {
     addToCart(product);
   };
 
-  // table columns
+  const isEditing = (record: Product) => record.id === editingKey;
+
+  const edit = (record: Partial<Product> & { id: React.Key }) => {
+    form.setFieldsValue({ ...record });
+    setEditingKey(record.id);
+  };
+
+  const cancel = () => {
+    setEditingKey('');
+  };
+
+  const save = async (id: number) => {
+    try {
+      const row = await form.validateFields();
+      const updatedProduct = { ...row, id } as Product;
+      await editProduct(updatedProduct);
+      setEditingKey('');
+    } catch (errInfo) {
+      console.log('Validate Failed:', errInfo);
+    }
+  };
+
   const columns = [
     {
       title: 'ID',
       dataIndex: 'id',
       key: 'id',
       width: '5%',
-      editable: true
+      editable: false,
     },
     {
       title: 'Title',
       dataIndex: 'title',
       key: 'title',
-      width: '25%',
+      width: '20%',
       editable: true,
       filters: products.map((product) => ({
         text: product.title,
         value: product.title,
       })),
-      onFilter: (value: any, record: { title: string | any[]; }) => record.title.includes(value),
+      onFilter: (value: string | number | boolean, record: Product) => record.title.includes(value as string),
       filterSearch: true,
     },
     {
@@ -79,72 +100,122 @@ const ProductsComponent: React.FC = () => {
       dataIndex: 'price',
       key: 'price',
       width: '10%',
-      sorter: (a: { price: number; }, b: { price: number; }) => a.price - b.price,
+      editable: true,
+      sorter: (a: Product, b: Product) => a.price - b.price,
+      render: (price: number) => <span>{price}</span>,
     },
     {
       title: 'Description',
       dataIndex: 'description',
       key: 'description',
-      width: '30%',
+      width: '35%',
+      editable: true,
     },
     {
       title: 'Images',
       dataIndex: 'images',
       key: 'images',
-      width: '20%',
+      width: '15%',
+      editable: false,
       render: (images: string[]) => Array.isArray(images) ? images.join(', ') : '',
     },
     {
       title: 'Action',
       key: 'action',
-      width: '10%',
       ellipsis: true,
-      render: (_: any, record: Product) => (
-        <div className='flex gap-2 justify-center items-center'>
-          <Button onClick={() => handleAddToCart(record)}>
-          <ShoppingCartOutlined />
-          </Button>
-          <Button type='dashed' >
-            Edit
-          </Button>
-        </div>
-      ),
+      render: (_: any, record: Product) => {
+        const editable = isEditing(record);
+        return (
+          <div>
+            {editable ? (
+              <>
+                <span>
+                  <Typography.Link onClick={() => save(record.id)} style={{ marginRight: 8 }}>
+                    Save
+                  </Typography.Link>
+                  <Popconfirm title="Sure to cancel?" onConfirm={cancel}>
+                    <a>Cancel</a>
+                  </Popconfirm>
+                </span>
+              </>
+            ) : (
+              <div className='flex gap-2 justify-center items-center'>
+                <Button disabled={editingKey !== ''} onClick={() => edit(record)}>
+                  Edit
+                </Button>
+                <Button onClick={() => handleAddToCart(record)}>
+                  <ShoppingCartOutlined />
+                </Button>
+              </div>
+            )}
+          </div>
+        );
+      },
     },
   ];
 
-  const onChange = (sorter: any, filters: any) => {
-    console.log('params', sorter, filters);
+  const mergedColumns = columns.map((col) => {
+    if (!col.editable) {
+      return col;
+    }
+    return {
+      ...col,
+      onCell: (record: Product) => ({
+        record,
+        inputType: col.dataIndex === 'price' ? 'number' : 'text',
+        dataIndex: col.dataIndex,
+        title: col.title,
+        editing: isEditing(record),
+      }),
+    };
+  });
+
+  const onChange = (pagination: any, filters: any, sorter: any) => {
+    console.log('params', pagination, filters, sorter);
   };
-  
 
   return (
     <div>
       <div className='flex justify-between items-center'>
-      <Button onClick={handleCreate} type="primary" className='mb-4 mt-4'>
-        Create Product
-      </Button>
-
-      <Button>
-      <ShoppingCartOutlined onClick={handleCart} />
-      </Button>
+        <Button onClick={handleCreate} type="primary" className='mb-4 mt-4'>
+          Create Product
+        </Button>
+        <Badge count={cart.reduce((total, item) => total + item.quantity, 0)}>
+          <Button onClick={handleCart} type="primary" shape="circle" icon={<ShoppingCartOutlined />} />
+        </Badge>
       </div>
       <div className='flex justify-center items-center min-h-96'>
-      {isLoading ? (
-        <Spin tip="Loading..." />
-      ) : (
-        <Table columns={columns}  dataSource={products} rowKey="id" onChange={onChange} />
-      )}
+        {isLoading ? (
+          <Spin tip="Loading..." />
+        ) : (
+          <Form form={form} component={false}>
+            <Table<Product>
+              components={{
+                body: {
+                  cell: EditableCell,
+                },
+              }}
+              bordered
+              dataSource={products}
+              columns={mergedColumns as any}
+              rowClassName="editable-row"
+              pagination={{ pageSize: 5, onChange: cancel }}
+              rowKey={(record) => record.id}
+              onChange={onChange}
+            />
+          </Form>
+        )}
       </div>
       <Modal
         title="Create Product"
-        visible={isModalVisible}
+        open={isModalVisible}
         onCancel={handleModalClose}
         footer={null}
       >
         <ProductForm onSubmit={handleFormSubmit} />
       </Modal>
       <Modal
-        visible={isCartVisible}
+        open={isCartVisible}
         onCancel={handleCartClose}
         footer={null}
       >
--- types/product.ts
+++
@@ -1,9 +0,0 @@
-export interface Product {
-    id: number;
-    title: string;
-    price: number;
-    description: string;
-    categoryId: number;
-    images: string[];
-    sorter: number;
-  }
